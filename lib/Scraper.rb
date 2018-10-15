e3d9c7f0486364e79d7356ef842ffee287b84d8c
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-15 21:54:38 +0000

    adding specific provider name, qualification and title to a return array of hashes

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -17,23 +17,38 @@ class Scraper
 
     team_specialties_languages = doc.css(".middleColumn_three").css('ul').first.text
     t_s_l_array = team_specialties_languages.split("\n").reject { |n| n == "" }
-    #sorted_t_s_l_array
-    sorted_t_s_l_array = t_s_l_array.map do |ele|
+
+    cleaned_t_s_l_array = t_s_l_array.map do |ele|
       ele.gsub(/\w+[:]/, "").strip
-      #ele.reject(/\w+[:]/, "").strip
+    end
+
+    team = cleaned_t_s_l_array[0]
+    specialties = cleaned_t_s_l_array[1]
+    languages = cleaned_t_s_l_array[2]
+
+
+    attr_hash = Hash.new
+    array_of_providers = Array.new
+
+
+    #getting name, qualification and title attributes 
+    doc.css(".middleColumn_three").css('h3').each do |provider|
+      new_hash = Hash.new
+      team_specialties_languages = provider.text.split(",")
+      new_hash[:name] = team_specialties_languages[0].strip
+      new_hash[:qualification] = team_specialties_languages[1].strip
+      new_hash[:title] = team_specialties_languages[1].strip
+      array_of_providers << new_hash
     end
 
 
 
-    #specialties
-    #languages
-
-    binding.pry
+    #binding.pry
 
 
-    attr_hash = Hash.new
 
-    #doc.css('.')
+
+
 
   end
 
