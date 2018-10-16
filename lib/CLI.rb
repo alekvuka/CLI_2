7b0fc0638101db7fc11e90e6e906cb9702b8f827
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-16 02:12:50 +0000

    cleaning up code

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -5,7 +5,6 @@ require_relative "Providers.rb"
 
 class CLI
 
-
   def initialize(clinic_url = "http://callen-lorde.org/meet-our-providers/")
     Scraper.scrape_page(clinic_url)
   end
@@ -22,23 +21,23 @@ class CLI
     user_input = gets.strip.to_i
 
 
-    if valid?(user_input) == true
-      if user_input == 1
-        choice_1
-      elsif user_input == 2
-        choice_2
-      elsif user_input == 3
-        choice_3
-      else
-        choice_4
-      end
+    valid?(user_input)
+
+    if user_input == 1
+      choice_1
+    elsif user_input == 2
+      choice_2
+    elsif user_input == 3
+      choice_3
+    else
+      choice_4
     end
+    
     start
 
   end
 
   def choice_1
-
 
     name_array = Providers.all.map do |provider|
               provider.name
@@ -53,12 +52,9 @@ class CLI
     puts "Which provider would you like to know more about?"
     user_input = gets.strip
 
-
-
     all_providers = Providers.all
     req_provider = all_providers.detect do |provider|
       user_input == provider.name
-      #binding.pry
       end
 
     return_validator(req_provider)
@@ -66,7 +62,7 @@ class CLI
     puts "=============================="
     if req_provider.team != nil
       puts "#{req_provider.name}'s team: #{req_provider.team}"
-    end 
+    end
     puts "#{req_provider.name}'s specialties: #{req_provider.specialties}"
     puts "#{req_provider.name}'s languages: #{req_provider.languages}"
     puts "#{req_provider.name}'s qualifications: #{req_provider.qualifications}"
@@ -75,14 +71,13 @@ class CLI
   end
 
   def choice_3
+
     puts "From what team would you like to get a list of providers (Orange, Green, Purple, Blue)"
     user_input = gets.strip
 
-    all_providers = Providers.all
-
     return_array = Array.new
 
-    all_providers.each do |provider|
+    Providers.all.each do |provider|
       if provider.team == user_input
         return_array << provider.name
       end
@@ -94,19 +89,16 @@ class CLI
   end
 
   def choice_4
+
     puts "From what specialty: Adolescent Health, Family Practice, HIV, Adult Primary Care or Internal Medicine"
     user_input = gets.strip
 
-    all_providers = Providers.all
-
     return_array = Array.new
 
-    all_providers.each do |provider|
+    Providers.all.each do |provider|
       temp_arr = provider.specialties.split(",")
-
       i = 0
       while i < temp_arr.size
-        #binding.pry
         if temp_arr[i].strip == user_input
           return_array << provider.name
         end
@@ -119,19 +111,22 @@ class CLI
 
   end
 
+#=================== Helper Methods ====================
+
   def valid?(user_input)
     if user_input == 1 || user_input == 2 || user_input == 3 || user_input == 4
       true
     else
-      false
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      puts "!!!!!!!!!!!!!  Please enter either 1, 2, 3 or 4  !!!!!!!!!!!!"
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      start
     end
   end
 
   def return_validator(array_or_hash)
 
-    #binding.pry
-
-    if array_or_hash == nil #|| array_or_hash.any?
+    if array_or_hash == nil
       puts "======================================================================================================"
       puts "!!!!!!!!   The doctor, team or specialty that you have choosen does not exit in this clinic   !!!!!!!!"
       puts "======================================================================================================"
@@ -144,16 +139,11 @@ class CLI
       start
 
     end
-
-
-
   end
 
   def printer(arry_to_print)
 
-
     puts "<<<<<<<<<<<<                HERE IS THE LIST:                             >>>>>>>>>>>"
-
     i = 0
     while i < arry_to_print.size
       puts arry_to_print[i]
