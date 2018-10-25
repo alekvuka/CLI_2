16115be76ad8227b9552ad1964e85e699c6b37fa
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-25 17:59:56 +0000

    move environment.rb from bin to lib folder

---
+++ bin/environment.rb
@@ -0,0 +1,8 @@
+require_relative "../lib/CLI"
+require_relative "../lib/Scraper.rb"
+require_relative "../lib/Provider.rb"
+require_relative "../lib/Printer.rb"
+require_relative "../lib/UserChoice.rb"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
--- lib/environment.rb
+++
@@ -1,8 +0,0 @@
-require_relative "CLI"
-require_relative "Scraper.rb"
-require_relative "Provider.rb"
-require_relative "Printer.rb"
-require_relative "UserChoice.rb"
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
